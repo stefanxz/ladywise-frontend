c4e6c6ecad9a69b2eb6fb9e9023078fadc77a16f
davidmrc6 <mrcdavid6@gmail.com>
2025-10-03 18:22:09 +0200

    chore: add prettier

--- app/index.tsx
+++ app/index.tsx
@@ -3,10 +3,10 @@ import "../assets/styles/main.css";
 
 export default function Index() {
   return (
-    <View
-        className="flex flex-col items-center justify-center h-screen overflow-y-scroll"
-    >
-      <Text className="font-semibold text-xl">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex flex-col items-center justify-center h-screen overflow-y-scroll">
+      <Text className="font-semibold text-xl">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
