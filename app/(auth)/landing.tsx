23aefbb90fa824c098076553e653e9a2ca708bf5
E. <e.a.oborzynska@student.tue.nl>
2025-10-08 23:39:30 +0200

    refactor: adding "ThemedPressable" component

--- app/(auth)/landing.tsx
+++ app/(auth)/landing.tsx
@@ -1,8 +1,8 @@
+import { ThemedPressable } from '@/components/ThemedPressable/ThemedPressable';
 import { useRouter } from 'expo-router';
 import React from 'react';
-import { Image, ImageBackground, Text, View } from 'react-native';
+import { Image, ImageBackground, Pressable, Text, View } from 'react-native';
 import '../../assets/styles/main.css';
-import AuthActions from '../../src/components/AuthActions';
 
 const logoApp = require("../../assets/images/Elegant-Ladybug-Woman-Logo-Design2.png");
 const backgroundPart1 = require("../../assets/images/LandingPage-background-part1.png");
@@ -25,20 +25,19 @@ const LandingPage = () => {
     return (
       <View className="flex-1 flex-col">
         {/* Top 55% of the screen holds the Brand Header on a white background. */}
-        <View className="flex-[0.55] w-full bg-[#FFFFFF]">
-          <View>
-            <Image source={logoApp} className="w-64 h-64 mx-auto mt-20" />
-            <Text className="text-[#A45A6B] text-[36px] font-heading text-center">
-              LadyWise
-            </Text>
-            <Text className="text-black text-[16px] font-body text-center">
-              Your personal companion for
-            </Text>
-            <Text className="text-black text-[16px] font-body text-center">
-              menstrual health insights.
-            </Text>
-          </View>
+        <View className="flex-[0.55] justify-center bg-[#FFFFFF]">
+          <Image source={logoApp} className="w-64 h-64 mx-auto" />
+          <Text className="text-4xl font-aclonica-regular text-brand mx-auto">
+            LadyWise
+          </Text>
+          <Text
+            className="text-gray-600 mx-auto"
+            style={{ maxWidth: 190, textAlign: "center" }}
+          >
+            Your personal companion for menstrual health insights.
+          </Text>
         </View>
+
         {/* Bottom 45% of the screen for the wave image backgrounds and action buttons. */}
         <View className="flex-[0.45] w-full bg-[#FFFFFF] justify-center">
           <ImageBackground source={backgroundPart1} resizeMode="stretch"  className="absolute inset-0">
@@ -50,8 +49,20 @@ const LandingPage = () => {
             <View className="flex-[0.75] w-full ">
               <ImageBackground source={backgroundPart2} resizeMode="stretch" className="absolute inset-0" >
                 {/* Pass the navigation functions as props to the presentational component */}
-                <View className="flex-1 justify-center items-center">
-                  <AuthActions onGetStarted={handleGetStarted} onLogin={handleLogin} />
+                <View className="flex-1 justify-center absolute bottom-24 left-0 right-0 items-center ">
+                  <ThemedPressable
+                    label="Register"
+                    onPress={handleGetStarted}
+                    loading={false}
+                    disabled={false}
+                    className="mt-18 w-80 self-center bg-brand"
+                  />
+                  <View className="flex-row items-center justify-center space-x-2 mt-4">
+                    <Text className="text-gray-600">Already have an account? </Text>
+                    <Pressable onPress={handleLogin}>
+                      <Text className="text-[#A45A6B] font-body">Log In</Text>
+                    </Pressable>
+                  </View>
                 </View>
               </ImageBackground>
             </View>
