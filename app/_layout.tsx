d9395be2758d407fea70d30447fcf1bcc5ac3010
stefanxz <stefanavram55@gmail.com>
2025-10-10 14:26:07 +0200

    chore: commented code and explained current logic flow

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout() {
     Inter_400Regular,
     Inter_600SemiBold,
     Aclonica_400Regular,
-  });
+  }); // fontsLoaded is updated automatically (similar to calling useState()) once the fonts are loaded
 
-  const [appReadyStatus, setAppReadyStatus] = useState(false);
+  const [appReadyStatus, setAppReadyStatus] = useState(false); // hook to handle state of the status of the app
 
   useEffect(() => {
     async function prepare() {
@@ -25,30 +25,35 @@ export default function RootLayout() {
         // Take control of the splash screen
         await SplashScreen.preventAutoHideAsync();
 
-        // If the fontsLoaded is true (boolean auto updated once the useFonts() hook finished execution)
+        // If the fontsLoaded is true (boolean auto updated once the useFonts() hook finished execution), then we can fetch user data
         if (fontsLoaded) {
           const delay = (ms: number) =>
             new Promise((resolve) => setTimeout(resolve, ms));
-          await delay(3000);
+          await delay(3000); // PLACEHOLDER timer to mimic what happens when the app first fetches user data. The fetching data is to be implemented this
         }
       } catch (e) {
-        console.log(e);
+        console.error(
+          "Encountered Error at Application Setup during Splash Screen handling: ",
+          e
+        ); // Basic error handling
       } finally {
         if (fontsLoaded) {
+          // Current logic permits fontsLoaded to be set to true only once the app setup is complete (explained below)
           setAppReadyStatus(true);
         }
       }
     }
     prepare();
-  }, [fontsLoaded]);
+  }, [fontsLoaded]); // Changes on fontsLoaded since the useEffect() hook always executes before loading fonts (when RootLayout is mounted), meaning that this hook will execute twice
 
   useEffect(() => {
     if (appReadyStatus) {
       // Hide the splash screen once the app is ready
       SplashScreen.hideAsync();
     }
-  }, [appReadyStatus]);
+  }, [appReadyStatus]); // Also executes once
 
+  // Do not return anything if the app is not ready; currently looks bad bacuse of the delay, real delay would be 10x smaller, unovservable by human eye
   if (!appReadyStatus) {
     return null;
   }
