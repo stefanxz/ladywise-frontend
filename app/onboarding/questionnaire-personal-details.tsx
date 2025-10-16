9f8cf7c78c98c2210ef5b2eefea69a573c3345df
E. <e.a.oborzynska@student.tue.nl>
2025-10-16 15:12:48 +0200

    feat(first-time-questionnaire):  Add input field for 'age'

--- app/onboarding/questionnaire-personal-details.tsx
+++ app/onboarding/questionnaire-personal-details.tsx
@@ -1,9 +1,16 @@
+import { ThemedTextInput } from "@/components/ThemedTextInput/ThemedTextInput";
 import { useRouter } from "expo-router";
+import { useState } from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Questionnaire() {
   const router = useRouter();
+  const [age, setAge] = useState("");
+
+  // error messages
+  const [ageError, setAgeError] = useState<string | null>(null);
+  
 
   return (
     <SafeAreaView className="flex-1 bg-white items-center pt-[162px]">
@@ -16,7 +23,24 @@ export default function Questionnaire() {
         {
           "Tell us a bit about yourself so we can tailor your health insights."
         }
-        </Text>
+        </Text >
+        <View className="w-full mt-12">
+          <Text className="pr-8 text-lg text-gray-600 text-left leading-relaxed">Age</Text>
+          <ThemedTextInput
+          value={age}
+          onChangeText={(t: string) => {
+              setAge(t);
+              if (ageError) setAgeError(null);
+          }}
+          placeholderTextColor="gray"
+          className={`h-11 ${ageError ? "border border-red-500" : ""}`}
+          secureTextEntry={false}
+          placeholder="Your age"
+          />
+          {ageError ? (
+            <Text className="text-red-600 text-xs mt-1">{ageError}</Text>
+            ) : null}
+        </View>
       </View>
     </SafeAreaView>
   );
