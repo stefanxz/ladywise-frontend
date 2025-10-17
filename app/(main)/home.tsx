9c09833ca094b11c46951ca640b3a0d17f052c4f
stefanxz <stefanavram55@gmail.com>
2025-10-18 01:08:06 +0200

    feat: added passing of data from parent compoenent home to child comopnent Risk (card)

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -1,11 +1,29 @@
 import InsightsSection from "@/components/InsightsSection/InsightsSection";
-import React from "react";
+import { RiskData } from "@/lib/types/health";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+const MOCK_INSIGHTS: RiskData[] = [
+  {
+    id: "1",
+    title: "Thrombosis Risk",
+    level: "Medium",
+    description: "Some factors may raise clotting risk.",
+  },
+  {
+    id: "2",
+    title: "Anemia Risk",
+    level: "Low",
+    description: "Iron levels appear sufficient.",
+  },
+];
+
 const home = () => {
+  useEffect(() => {}, []);
+  const [data, setData] = useState<RiskData[]>(MOCK_INSIGHTS);
   return (
     <SafeAreaView>
-      <InsightsSection></InsightsSection>
+      <InsightsSection insights={data}></InsightsSection>
     </SafeAreaView>
   );
 };
--- components/InsightsSection/InsightsSection.tsx
+++ components/InsightsSection/InsightsSection.tsx
@@ -1,26 +1,21 @@
+import { RiskData } from "@/lib/types/health";
 import React from "react";
 import { Text, View } from "react-native";
 import RiskCard from "../RiskCard/RiskCard";
 
-const InsightsSection = () => {
+interface InsightsSectionProps {
+  insights: RiskData[];
+}
+const InsightsSection = ({ insights }: InsightsSectionProps) => {
   return (
     <View className="p-4 mt-9">
       <Text className="text-xl font-bold mb-4">Your insights</Text>
       <View className="flex-row gap-4">
-        <View className="flex-1">
-          <RiskCard
-            title="Thrombosis Risk"
-            level="Medium"
-            description="lorem ipsum"
-          ></RiskCard>
-        </View>
-        <View className="flex-1">
-          <RiskCard
-            title="Thrombosis Risk"
-            level="Low"
-            description="lorem ipsum"
-          ></RiskCard>
-        </View>
+        {insights.slice(0, 2).map((item: RiskData) => (
+          <View key={item.id} className="flex-1">
+            <RiskCard {...item} />
+          </View>
+        ))}
       </View>
     </View>
   );
