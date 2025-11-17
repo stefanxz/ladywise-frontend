6e64954bd1fc3a751442187ddb02096c6aea2f45
ilincagurgu <167888876+ilincagurgu@users.noreply.github.com>
2025-11-17 10:13:34 +0100

    fix linting error?

--- app/onboarding/first-questionnaire-completion.tsx
+++ app/onboarding/first-questionnaire-completion.tsx
@@ -61,7 +61,8 @@ export default function FirstQuestionnaireCompletion() {
               </Text>
 
             <Text className="text-lg text-gray-600 text-left leading-relaxed">
-              Thank you for sharing your details. LadyWise will now personalize your health insights based on your profile.
+                Thank you for sharing your details. LadyWise will now
+              personalize your health insights based on your profile.
             </Text>
             </View>
 
