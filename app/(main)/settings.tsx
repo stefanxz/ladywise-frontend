3d8f65774fbe6349054da10ae44ef0cf57576571
David Mereacre <mrcdavid6@gmail.com>
2025-12-02 15:53:58 +0100

    change theme of logout button to match app theme

--- app/(main)/settings.tsx
+++ app/(main)/settings.tsx
@@ -1,6 +1,8 @@
 import { useAuth } from "@/context/AuthContext";
-import { Button, Text } from "react-native";
+import { Button, ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedPressable } from "@/components/ThemedPressable/ThemedPressable";
+import React from "react";
 
 export default function SettingsScreen() {
   const { signOut } = useAuth();
@@ -11,11 +13,21 @@ export default function SettingsScreen() {
   };
   return (
     <>
-      <SafeAreaView className="flex-1 bg-background p-6">
+      <SafeAreaView className="flex-1 bg-background px-6">
         <Text className="text-headingText font-inter-semibold text-2xl">
           Settings
         </Text>
-        <Button title="Log Out" onPress={handleLogout} color="#ff3b30" />
+
+        {/* Settings content */}
+        <ScrollView className="flex-1 py-8 w-full">
+          <ThemedPressable
+            label="Log Out"
+            onPress={handleLogout}
+            testID="log-out-button"
+            loading={false}
+            disabled={false}
+          />
+        </ScrollView>
       </SafeAreaView>
     </>
   );
